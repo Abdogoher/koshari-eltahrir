ecc1ac1eba38c2903a47553ab004babe2701c58c
goher <goher@gmail.com>
2025-09-09 20:08:53 +0300

    edit-router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter  as Router, Routes, Route } from "react-router-dom";
 import Navbar from './Components/Utilites/Navbar';
 import HomePage from './Pages/HomePage';
 import Manu from './Pages/Manu';
